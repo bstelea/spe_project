d6dce72fdc29fa0574eb84ede5a887422d26f395
stelea <bs17580@my.bristol.ac.uk>
2018-11-26 12:19:12 +0200

    Starting development

---
+++ global-beer-shop/src/main/java/com/example/globalbeershop/GlobalBeerShopApplication.java
@@ -0,0 +1,12 @@
+package com.example.globalbeershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalBeerShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalBeerShopApplication.class, args);
+	}
+}
