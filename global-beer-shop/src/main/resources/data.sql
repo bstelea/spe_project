b639f2ef4babc7ea5b4d1fe03afe787e599d7966
charles figuero <clewisfiguero@gmail.com>
2018-11-28 17:39:36 +0000

    added more beers

--- global-beer-shop/src/main/resources/data.sql
+++ global-beer-shop/src/main/resources/data.sql
@@ -4,6 +4,25 @@ values(1,'Guinness', 'Ireland', 'Diageo', 'Stout', 4.2, NULL, NULL, NULL, NULL, 
 insert into beerStocked
 values(2,'Hobgoblin ', 'England', 'Wychwood Brewery', 'Ale', 5.2, NULL, NULL, NULL, NULL, NULL  );
 
+
+insert into beerStocked
+values(3, 'Carlsberg', 'Denmark', 'JD ltd.', 'Lager', 3.8, NULL, NULL, NULL, NULL, NUll);
+
+insert into beerStocked
+values (4, 'Fosters', 'Australia', 'Cuntingtons UTD', 50, null, null, null, null, null);
+
+insert into beerStocked
+values (5, 'Devils Backbone', 'Australia', 'Cuntingtons UTD', 50, null, null, null, null, null);
+
+insert into beerStocked
+values (6, 'Abbot Ale', 'Australia', 'Cuntingtons UTD', 50, null, null, null, null, null);
+
+insert into beerStocked
+values (7, 'Stella Artois', 'Australia', 'Cuntingtons UTD', 50, null, null, null, null, null);
+
+insert into beerStocked
+values (8, 'Peroni', 'Australia', 'Cuntingtons UTD', 50, null, null, null, null, null);
+
   -- id integer not null,
    --name varchar(255) not null,
   -- country varchar(255) not null,
