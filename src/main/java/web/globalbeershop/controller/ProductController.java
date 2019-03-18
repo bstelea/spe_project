45864786cf709a4f355eb69b675ded49362b1b83
unknown <bs17580@my.bristol.ac.uk>
2019-03-18 07:46:52 +0000

    product page added

--- src/main/java/web/globalbeershop/controller/ProductController.java
+++ src/main/java/web/globalbeershop/controller/ProductController.java
@@ -1,15 +1,26 @@
 package web.globalbeershop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.globalbeershop.service.BeerService;
 
 @Controller
 public class ProductController {
 
-    @GetMapping("/product")
-    public String product() {
-        return "product";
+    private final BeerService beerService;
+
+    @Autowired
+    public ProductController(BeerService beerService) {
+        this.beerService = beerService;
+    }
+
+    @GetMapping("/products/{id}")
+    public String product(Model model, @PathVariable Long id) {
+        model.addAttribute("beer", beerService.findById(id).get());
+        return "product-page";
     }
 }
