82f9163803d247353ab832ac4398c2fda1ad8cb3
unknown <bs17580@my.bristol.ac.uk>
2019-03-24 16:47:24 +0000

    still trying to make deploy work

--- src/main/java/web/globalbeershop/GlobalbeershopApplication.java
+++ src/main/java/web/globalbeershop/GlobalbeershopApplication.java
@@ -2,21 +2,21 @@ package web.globalbeershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.context.annotation.PropertySources;
 import web.globalbeershop.service.BraintreeGatewayService;
 import com.braintreegateway.BraintreeGateway;
 
 import java.io.File;
 
-@PropertySources({
-		@PropertySource(value = "classpath:secret.properties", ignoreResourceNotFound = true),
-		@PropertySource(value = "file:${user.home}/.secret.properties", ignoreResourceNotFound = true)
-})
+//@PropertySources({
+//		@PropertySource(value = "classpath:secret.properties", ignoreResourceNotFound = true),
+//		@PropertySource(value = "file:${user.home}/.secret.properties", ignoreResourceNotFound = true)
+//})
 @SpringBootApplication
 public class GlobalbeershopApplication {
 
-	public static String DEFAULT_CONFIG_FILENAME = "file:${user.home}/.config.properties";
+	public static String DEFAULT_CONFIG_FILENAME = "../config.properties";
 	public static BraintreeGateway gateway;
 
 	public static void main(String[] args) {
