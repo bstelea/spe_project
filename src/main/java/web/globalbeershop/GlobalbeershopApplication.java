2824e562999ae5b5b02f67473e2f21b1b65b450a
unknown <bs17580@my.bristol.ac.uk>
2019-03-24 19:40:54 +0000

    still and again

--- src/main/java/web/globalbeershop/GlobalbeershopApplication.java
+++ src/main/java/web/globalbeershop/GlobalbeershopApplication.java
@@ -17,13 +17,17 @@ import java.io.File;
 public class GlobalbeershopApplication {
 
 	public static String DEFAULT_CONFIG_FILENAME = "../config.properties";
+	public static String DEFAULT_CONFIG_FILENAME_FOR_DEV = "config.properties";
 	public static BraintreeGateway gateway;
 
 	public static void main(String[] args) {
 		File configFile = new File(DEFAULT_CONFIG_FILENAME);
+		File devFile = new File(DEFAULT_CONFIG_FILENAME_FOR_DEV);
 		try {
 			if(configFile.exists() && !configFile.isDirectory()) {
 				gateway = BraintreeGatewayService.fromConfigFile(configFile);
+			} else if (devFile.exists() && !devFile.isDirectory()) {
+				gateway = BraintreeGatewayService.fromConfigFile(devFile);
 			} else {
 				gateway = BraintreeGatewayService.fromConfigMapping(System.getenv());
 			}
