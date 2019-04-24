209046af8433f052cd20c7f76af91630d6e0fae8
AngusParsonson <amfparsonson@gmail.com>
2019-04-24 14:41:58 +0100

    product controller with mockmvc test

--- src/test/java/web/globalbeershop/ProductControllerTest.java
+++ src/test/java/web/globalbeershop/ProductControllerTest.java
@@ -8,24 +8,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import web.globalbeershop.controller.CustomErrorController;
+import web.globalbeershop.controller.ProductController;
 
 import javax.servlet.ServletContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(
         classes = {GlobalbeershopApplication.class}
 )
 @WebAppConfiguration
-public class CustomErrorControllerTest {
+public class ProductControllerTest {
 
     private MockMvc mockMvc;
 
@@ -46,8 +53,21 @@ public class CustomErrorControllerTest {
     }
 
     @Test
-    public void handleErrorForbiddenValueTest() {
-        
+    public void showsCorrectViewName() throws Exception{
+        mockMvc.perform( MockMvcRequestBuilders
+                .get("/products/{id}", 1)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.view().name("product-page")).andReturn();
     }
 
+    @Test
+    public void checkContentTypeOfResponseIsCorrectTest() throws Exception{
+        MvcResult mvcResult= mockMvc.perform( MockMvcRequestBuilders
+                .get("/products/{id}", 1)
+                .accept(MediaType.APPLICATION_JSON)).andReturn();
+
+        Assert.assertEquals("text/html;charset=UTF-8",
+                mvcResult.getResponse().getContentType());
+    }
 }
