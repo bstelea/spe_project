c3b59d1910fac60117fedac6a1460452dec435cf
unknown <bs17580@my.bristol.ac.uk>
2019-04-30 01:45:07 +0100

    added scroller and modified homepage details and changed order complete email subject and added BeerTest to circleci

--- src/main/java/web/globalbeershop/service/NotificationService.java
+++ src/main/java/web/globalbeershop/service/NotificationService.java
@@ -41,17 +41,8 @@ public class NotificationService {
         SimpleMailMessage mail = new SimpleMailMessage();
         mail.setTo(email);
         mail.setFrom("globalbeershopmail@gmail.com");
-        mail.setSubject("WARNING");
-//        mail.setText("Dear Mr Raf, \n" +
-//                "Error: Loading class `com.mysql.jdbc.Driver'. This is deprecated. The new driver class is `com.mysql.cj.jdbc.Driver'. The driver is automatically registered via the SPI and manual loading of the driver class is generally unnecessary.\n\n\n\n" +
-//                "JK it worked\n\n" +
-//                "The email you are reading at this present moment in time is not just a test but a celebration of the great work achieved over the last 3 months. " +
-//                "Although you may simply see words on a screen, please believe me when I say this message hides unbounded levels of complexity and programming skill." +
-//                "In fact the greatest minds of our generation have sometimes described this email service as a symphony of placid beauty.\n \n" +
-//                "It is my greatest hope that this item of mail does reach you, however one can argue the true genius of this sacred work is that you won't know if it doesn't work.\n\n" +
-//                "Your's truly,\n" +
-//                "Charles Lewis Figuero, Architect and Friend.");
-        mail.setText("Order Complete.");
+        mail.setSubject("Order Placed");
+        mail.setText("Thank you for your order!");
 
 
         javaMailSender.send(mail);
--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -2,5 +2,33 @@ jQuery(document).ready(function() {
 	
 	"use strict";
 	// Your custom js code goes here.
+	var goUnderSlider = function() {
+
+		$('.js-gounder').on('click', function(event){
+
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $('#under-slider').offset().top
+			}, 500, 'easeInOutExpo');
+
+			return false;
+		});
+
+		$(window).scroll(function(){
+			var $win = $(window);
+			if ($win.scrollTop() > 200) {
+				$('.js-under').addClass('active');
+			} else {
+				$('.js-under').removeClass('active');
+			}
+
+		});
+
+	};
+
+	$(document).ready(function(){
+		goUnderSlider();
+	});
 
 });
